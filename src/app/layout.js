302b1fa433e82f6ec50ccd7222b45bdc1ddc5891
nwin19 <nandarwinbuma@gmail.com>
2024-03-05 19:06:19 +0630

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,20 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Next App",
+  description: "Next.js starter app",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <h1>This is the main layout</h1>
+        {children}
+      </body>
+    </html>
+  );
+}
